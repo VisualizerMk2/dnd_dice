8c340721e40dce93e7b58a1fd998a459bcccecbe
Aaron Barnett <aaronsaccts@gmail.com>
2018-09-21 18:41:10 -0500

    added docstring for parse_dice_roll

--- dnd_dice.py
+++ dnd_dice.py
@@ -3,8 +3,10 @@ import random
 user_input = raw_input("Enter your roll: ")
 
 def parse_dice_roll(user_dice_roll):
+    """ Takes the user input and parses out the number of dice to be rolled
+    and how many sides the dice have, and also takes in account any modifiers. """
     try:
-        quantity_of_dice, number_of_sides = user_dice_roll.split('d')
+        quantity_of_dice, dice_and_mod_string = user_dice_roll.split('d')
         quantity_of_dice = int(quantity_of_dice)
         number_of_sides = int(number_of_sides)
         print_rolls(quantity_of_dice, number_of_sides)
@@ -15,6 +17,8 @@ def print_rolls(quantity_of_dice, number_of_sides):
     for x in range(quantity_of_dice - 1):
         print "%d +" % random.randint(1,number_of_sides),
     print "%d" % random.randint(1,number_of_sides)
+
+def parse_dice_and_modifier():
 
 
 parse_dice_roll(user_input)

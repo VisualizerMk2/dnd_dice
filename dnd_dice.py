f65711dc47519b289e6c51fb3a51347f1e86ad4f
Aaron Barnett <aaronsaccts@gmail.com>
2018-09-23 16:00:36 -0500

    added function to parse the num of sides and modifier from input

--- dnd_dice.py
+++ dnd_dice.py
@@ -8,7 +8,7 @@ def parse_dice_roll(user_dice_roll):
     try:
         quantity_of_dice, dice_and_mod_string = user_dice_roll.split('d')
         quantity_of_dice = int(quantity_of_dice)
-        number_of_sides = int(number_of_sides)
+        number_of_sides, mod_sign = parse_dice_and_modifier(dice_and_mod_string)
         print_rolls(quantity_of_dice, number_of_sides)
     except ValueError:
         print "Please type in rolls in the format '1d6'."
@@ -18,7 +18,10 @@ def print_rolls(quantity_of_dice, number_of_sides):
         print "%d +" % random.randint(1,number_of_sides),
     print "%d" % random.randint(1,number_of_sides)
 
-def parse_dice_and_modifier():
-
+def parse_dice_and_modifier(dice_mod_string):
+    num_sides, mod_string = dice_mod_string.split()
+    num_sides = int(num_sides)
+    mod_sign = mod_string[0]
+    return num_sides, mod_sign
 
 parse_dice_roll(user_input)

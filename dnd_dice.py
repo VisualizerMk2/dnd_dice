447cce616b3b811f79a714b2db3e30a474ea0327
Aaron Barnett <aaronsaccts@gmail.com>
2018-10-12 19:12:08 -0500

    fixed regex string, added check for correct user input

--- dnd_dice.py
+++ dnd_dice.py
@@ -4,12 +4,20 @@ import re
 user_input = raw_input("Enter your roll: ")
 
 def check_input_format(user_dice_roll):
-    #Regex: The order goes at least one digit -> 'd' char -> at least one digit ->
+    #Regex: The order goes: at least one digit -> 'd' char -> at least one digit ->
     #   spaces -> optional '+' or '-' -> spaces -> digits -> spaces
     #Spaces are not captured for matching
     #Should be an object of 5 matches
-    regex = r"^(\d+)(d)(\d+)(?:[\s]*)(\+ | \-)?(?:[\s]*)(\d+)?(?:[\s]*)$"
+
+    regex = re.compile(r'^(\d+)(d)(\d+)(?:[\s]*)?(\+|\-)?(?:[\s]*)(\d+)?(?:[\s]*)$')
     regex_matches = re.match(regex, user_dice_roll)
+
+    #Check for correct input format. If its correct, return the match objectself.
+    #   If its incorrect, print a message prompting for re-entry
+    if regex_matches == None:
+        print "Please enter rolls in the format '2d10 + 3'."
+    else:
+        return regex_matches
 
 def parse_dice_roll(user_dice_roll):
     """ Takes the user input and parses out the number of dice to be rolled

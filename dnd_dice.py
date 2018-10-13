a79166ec1895e18aed650a2307ba013d291b54a0
Aaron Barnett <aaronsaccts@gmail.com>
2018-10-12 19:56:25 -0500

    deleted unnecessary funcs

--- dnd_dice.py
+++ dnd_dice.py
@@ -4,7 +4,7 @@ import re
 user_input = raw_input("Enter your roll: ")
 
 def check_input_format(user_dice_roll):
-    """ Takes the user input and matches it against a regex. 
+    """ Takes the user input and matches it against a regex.
     Incorrectly formatted rolls will generate an error message.
 
     Input: A string of the user's inputted dice roll.
@@ -24,17 +24,6 @@ def check_input_format(user_dice_roll):
     else:
         return regex_matches
 
-def parse_dice_roll(user_dice_roll):
-    """ Takes the user input and parses out the number of dice to be rolled
-    and how many sides the dice have, and also takes in account any modifiers. """
-    try:
-        quantity_of_dice, dice_and_mod_string = user_dice_roll.split('d')
-        quantity_of_dice = int(quantity_of_dice)
-        number_of_sides, mod_sign = parse_dice_and_modifier(dice_and_mod_string)
-        print_rolls(quantity_of_dice, number_of_sides)
-    except ValueError:
-        print "Please type in rolls in the format '1d6'."
-
 def create_dice_rolls(quantity_of_dice, number_of_sides):
     rolls = []
     for x in range(quantity_of_dice):
@@ -45,13 +34,3 @@ def print_rolls(rolls):
     for x in rolls:
         print x
     print '---'
-
-def parse_dice_and_modifier(dice_mod_string):
-    num_sides, mod_string = dice_mod_string.split()
-    num_sides = int(num_sides)
-    mod_sign = mod_string[0]
-    modifier = mod_string[1]
-    modifier = int(modifier)
-    return num_sides, mod_sign, modifier
-
-parse_dice_roll(user_input)

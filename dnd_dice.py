695c203caa0562f58e7411ced6c9ad192b99dabc
Aaron Barnett <aaronsaccts@gmail.com>
2018-10-12 21:23:21 -0500

    testing new return val for check_input_format

--- dnd_dice.py
+++ dnd_dice.py
@@ -20,7 +20,7 @@ def check_input_format(user_dice_roll):
     if regex_matches == None:
         print "Please enter rolls in the format '2d10 + 3'."
     else:
-        return regex_matches
+        return roll_regex_obj[1], roll_regex_obj[3], roll_regex_obj[4], roll_regex_obj[5]
 
 def create_dice_rolls(quantity_of_dice, number_of_sides):
     rolls = []
@@ -37,13 +37,12 @@ user_input = raw_input("Enter your roll: ")
 
 print "User input is: ", user_input
 
-roll_regex_obj = check_input_format(user_input)
+quant_dice, num_sides, modifier_sign, modifier_val = check_input_format(user_input)
 
 print roll_regex_obj
 
-quant_dice = int(roll_regex_obj[1])
-num_sides = int(roll_regex_obj[3])
-modifier_sign = roll_regex_obj[4]
-modifier_val = int(roll_regex_obj[5])
+quant_dice = int(quant_dice)
+num_sides = int(num_sides)
+modifier_val = int(modifier_val)
 
 rolls = create_dice_rolls(quant_dice, num_sides)
